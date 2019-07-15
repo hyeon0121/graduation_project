9cf380578ba8581acab78087328d2aa235ac2c2e
2015005050 <dpwl1172@naver.com>
2019-07-15 15:23:51 +0900

    Add CoapResource

--- src/main/java/com/example/demo/CoAPServerBase.java
+++ src/main/java/com/example/demo/CoAPServerBase.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.resource.ConnectResource;
+import com.example.demo.resource.*;
 import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.CoapServer;
-import com.example.demo.global.Global;
+import com.example.demo.global.CoAPGlobal;
 
 public class CoAPServerBase {
 
@@ -14,8 +14,13 @@ public class CoAPServerBase {
 		server.add(new ConnectResource("connect"));
 
 		CoapResource observe_res = new CoapResource("obs");
-		Global.setObserve_resource(observe_res);
+		CoAPGlobal.setObserve_resource(observe_res);
 		server.add(observe_res);
+
+		server.add(new GetResource("get"));
+		server.add(new PutResource("put"));
+		server.add(new PostResource("post"));
+		server.add(new DeleteResource("delete"));
 
 		server.start();
 
