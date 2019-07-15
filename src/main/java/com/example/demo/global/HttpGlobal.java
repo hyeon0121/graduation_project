535b47abcc2bfeb75cc1f041b951d0bfb9f93151
2015005050 <dpwl1172@naver.com>
2019-07-15 15:47:32 +0900

    Assigning variables for web client test

---
+++ src/main/java/com/example/demo/global/HttpGlobal.java
@@ -0,0 +1,12 @@
+package com.example.demo.global;
+
+public class HttpGlobal {
+    public static String[] proverbArr = {"The grass is always greener on the other side of the fence.",
+            "Don’t judge a book by its cover.","Strike while the iron is hot.", "Too many cooks spoil the broth.",
+            " You can’t have your cake and eat it too."};
+
+    public static String postAnswer;
+    public static String getAnswer;
+
+    public static int port;
+}
