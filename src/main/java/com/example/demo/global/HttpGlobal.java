6d71f78e2844edd1de6fcf4dc2b99fcad91012ca
2015005050 <dpwl1172@naver.com>
2019-07-22 12:46:07 +0900

    Assign global variables to score web client scenario test

--- src/main/java/com/example/demo/global/HttpGlobal.java
+++ src/main/java/com/example/demo/global/HttpGlobal.java
@@ -2,11 +2,14 @@ package com.example.demo.global;
 
 public class HttpGlobal {
     public static String[] proverbArr = {"The grass is always greener on the other side of the fence.",
-            "Don’t judge a book by its cover.","Strike while the iron is hot.", "Too many cooks spoil the broth.",
-            " You can’t have your cake and eat it too."};
+            "Don’t judge a book by its cover.", "Strike while the iron is hot.", "Too many cooks spoil the broth.",
+            "You can’t have your cake and eat it too."};
 
+    public static int port;
+    public static boolean httpCheck = false;
+    public static boolean httpVersion = false;
+    public static boolean headerUserAgent = false;
     public static String postAnswer;
     public static String getAnswer;
 
-    public static int port;
 }
