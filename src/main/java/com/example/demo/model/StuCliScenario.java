9b013e1cf790ae2310218fb45d1b3355ff2d4f00
2015005050 <dpwl1172@naver.com>
2019-08-17 13:58:50 +0900

    Add web client scenario test class

---
+++ src/main/java/com/example/demo/model/StuCliScenario.java
@@ -0,0 +1,65 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+public class StuCliScenario extends Student {
+    String getAnswer, postAnswer;
+    boolean httpCheck, httpVersion, userAgent;
+    Date accessTime;
+
+    public StuCliScenario(String sname, String sno, String sip, String sport, boolean httpCheck, boolean httpVersion, boolean userAgent, Date accessTime) {
+        super(sname, sno, sip, sport);
+        this.httpCheck = httpCheck;
+        this.httpVersion = httpVersion;
+        this.userAgent = userAgent;
+        this.accessTime = accessTime;
+    }
+
+    public String getGetAnswer() {
+        return getAnswer;
+    }
+
+    public void setGetAnswer(String getAnswer) {
+        this.getAnswer = getAnswer;
+    }
+
+    public String getPostAnswer() {
+        return postAnswer;
+    }
+
+    public void setPostAnswer(String postAnswer) {
+        this.postAnswer = postAnswer;
+    }
+
+    public boolean isHttpCheck() {
+        return httpCheck;
+    }
+
+    public void setHttpCheck(boolean httpCheck) {
+        this.httpCheck = httpCheck;
+    }
+
+    public boolean isHttpVersion() {
+        return httpVersion;
+    }
+
+    public void setHttpVersion(boolean httpVersion) {
+        this.httpVersion = httpVersion;
+    }
+
+    public boolean isUserAgent() {
+        return userAgent;
+    }
+
+    public void setUserAgent(boolean userAgent) {
+        this.userAgent = userAgent;
+    }
+
+    public Date getAccessTime() {
+        return accessTime;
+    }
+
+    public void setAccessTime(Date accessTime) {
+        this.accessTime = accessTime;
+    }
+}
