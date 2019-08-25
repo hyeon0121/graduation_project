b3ab8a4ac0dd58262b4ab8936d80650d538eff76
2015005050 <dpwl1172@naver.com>
2019-08-25 14:31:46 +0900

    Add variables considering db

--- src/main/java/com/example/demo/global/HttpGlobal.java
+++ src/main/java/com/example/demo/global/HttpGlobal.java
@@ -1,5 +1,12 @@
 package com.example.demo.global;
 
+import com.example.demo.model.StuCliGet;
+import com.example.demo.model.StuCliPost;
+import com.example.demo.model.StuCliScenario;
+import com.example.demo.model.StuServerInfo;
+
+import java.util.HashMap;
+
 public class HttpGlobal {
     public static String[] proverbArr = {"The grass is always greener on the other side of the fence.",
             "Don’t judge a book by its cover.", "Strike while the iron is hot.", "Too many cooks spoil the broth.",
@@ -11,5 +18,15 @@ public class HttpGlobal {
     public static boolean headerUserAgent = false;
     public static String postAnswer;
     public static String getAnswer;
+
+    public static HashMap<String, StuCliScenario> webCliStuInfo = new HashMap<String, StuCliScenario>();
+    public static HashMap<String, StuCliScenario> webCliSceStuInfo = new HashMap<String, StuCliScenario>();
+    public static HashMap<String, StuCliGet> webCliGetStuInfo = new HashMap<String, StuCliGet>();
+    public static HashMap<String, StuCliPost> webCliPostStuInfo = new HashMap<String, StuCliPost>();
+
+    public static HashMap<String, Integer> statusMap = new HashMap<>();
+    public static HashMap<String, StuServerInfo> stuInfo = new HashMap<>();
+
+
 
 }
